2a95ae4dc8a24c0010d8820115b8ad7db7b85929
judygab <ivditigabeskiria@gmail.com>
2023-08-15 17:11:55 -0700

    added mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
@@ -22,14 +25,38 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
       <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href="/" className="text-5xl text-white font-semibold">
+        <Link
+          href="/"
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
           LOGO
         </Link>
-        <div class="hidden md:block md:w-auto" id="navbar-default">
-          <ul class="font-medium flex p-4 md:p-0  rounded-lg flex-row md:space-x-8 mt-0">
+        <div className="block md:hidden">
+          {!navbarOpen ? (
+            <button
+              id="nav-toggle"
+              className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
+              onClick={() => setNavbarOpen(true)}
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              id="nav-toggle"
+              className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
+              onClick={() => setNavbarOpen(false)}
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="hidden md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex p-4 md:p-0  rounded-lg flex-row md:space-x-8 mt-0">
             {navLinks.map((link) => {
               return (
                 <li>
@@ -44,6 +71,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );
 };

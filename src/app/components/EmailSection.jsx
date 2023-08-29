ef4d76723ebad40065621168767191c39cfa31f8
judygab <ivditigabeskiria@gmail.com>
2023-08-29 18:06:00 -0400

    remove apostrophe

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -12,12 +12,14 @@ const EmailSection = () => {
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">
+          Let`&apos;s Connect
+        </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or just want to say hi, I'll try my best
-          to get back to you!
+          I'&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I'&apos;ll
+          try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="github.com">

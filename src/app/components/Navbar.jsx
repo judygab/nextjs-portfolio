e3c26b340d3ee86bc3a579a5659e7c2c06396f88
judygab <ivditigabeskiria@gmail.com>
2023-08-29 22:03:08 -0400

    deployment

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
               onClick={() => setNavbarOpen(false)}
               className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
-              <XmarkIcon className="h-5 w-5" />
+              <XMarkIcon className="h-5 w-5" />
             </button>
           )}
         </div>

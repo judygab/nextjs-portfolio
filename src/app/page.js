f1e58224ec406b2b0a34e03e75592f7ef0f5ff44
Hakan FIRAT <141479480+hknfrt@users.noreply.github.com>
2023-10-10 10:19:05 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";

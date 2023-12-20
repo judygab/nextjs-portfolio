8ac6aa4e4674e2103f52079bc414165fe6a52bc6
Afnan A. <hafnan03@gmail.com>
2023-12-21 00:07:37 +0500

    Font size

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container py-12 flex justify-between">
         {/* <span>LOGO</span> */}
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert ml-5 lg:ml-16 md:ml-16 sm:ml-12"
           src="/next.svg"
           alt="Next.js Logo"
           width={120}
           height={37}
           priority
         />
-        <p className="text-[#ADB7BE]">
+        <p className="text-[#ADB7BE] text-xs">
           &copy;{currentYear} All Rights Reserved.
         </p>
       </div>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,8 +15,8 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+          <h1 className="text-white mb-4 text-3xl sm:text-3xl lg:text-5xl lg:leading-normal font-extrabold">
+            <span className="text-transparent text-5xl sm:text-6xl lg:text-8xl bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>

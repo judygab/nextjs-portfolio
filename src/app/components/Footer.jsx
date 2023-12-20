e7f541addb612f0d856dd11d3322ba05711bccfd
Afnan A. <hafnan03@gmail.com>
2023-12-20 14:28:45 +0500

    Footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        {/* <span>LOGO</span> */}
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={120}
+          height={37}
+          priority
+        />
+        <p className="text-[#ADB7BE]">
+          &copy;{currentYear} All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
